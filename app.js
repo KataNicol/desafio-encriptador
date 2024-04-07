6b2e35bb7dcf6485b42980478659b0e51c5bdbe9
Kata <olivares.ossandon@gmail.com>
2024-04-07 00:51:18 -0400

    actualiza el app.js

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 
+document.querySelector()
+
+
+function encriptarTexto(cadena){
+    const mapeoVocales = {
+        'a': 'ai',
+        'e': 'enter',
+        'i': 'imes',
+        'o': 'ober',
+        'u': 'ufat',
+    };
+}
+
+// y luego las reemplazamos con la palabra correspondiente del mapeoVocales
+    return cadena.replace(/[aeiou]/g, function(vocal) {
+        return mapeoVocales[vocal];
+    });
+
+
+// Ejemplo de uso:
+let textoOriginal = "Este es un ejemplo de sustitución de vocales.";
+let textoModificado = sustituirVocales(textoOriginal);
+
+console.log(textoModificado);
+// Salida: "Estmanzana es un ejemplomple de sustitución de elefantelefanteres."
