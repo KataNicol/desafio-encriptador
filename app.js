dbbe44ab23bc7c14270bf4eb58ab09a10a954e77
Kata <olivares.ossandon@gmail.com>
2024-04-10 03:42:57 -0400

    termina el app.js

--- app.js
+++ app.js
@@ -1,26 +1,49 @@
+const textoOriginal = document.querySelector(".textoOriginal");
+const mensaje = document.querySelector(".mensaje");
 
-document.querySelector()
+
+//La letra "e" es convertida para "enter"
+//La letra "i" es convertida para "imes"
+//La letra "a" es convertida para "ai"
+//La letra "o" es convertida para "ober"
+//La letra "u" es convertida para "ufat"
 
 
-function encriptarTexto(cadena){
-    const mapeoVocales = {
-        'a': 'ai',
-        'e': 'enter',
-        'i': 'imes',
-        'o': 'ober',
-        'u': 'ufat',
-    };
+function btnEncriptar(){
+    const textoEncriptado = encriptarTexto(textoOriginal.value)
+    mensaje.value = textoEncriptado
+    textoOriginal.value = "";
+    mensaje.style.backgroundImage ="none"
 }
 
-// y luego las reemplazamos con la palabra correspondiente del mapeoVocales
-    return cadena.replace(/[aeiou]/g, function(vocal) {
-        return mapeoVocales[vocal];
-    });
+
+function encriptarTexto(stringEncriptada){
+    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1])
+        }
+    }
+    return stringEncriptada
+}
 
 
-// Ejemplo de uso:
-let textoOriginal = "Este es un ejemplo de sustitución de vocales.";
-let textoModificado = sustituirVocales(textoOriginal);
+function btnDesencriptar(){
+    const textoEncriptado = desencriptarTexto(textoOriginal.value)
+    mensaje.value = textoEncriptado
+    textoOriginal.value = "";
+    }
 
-console.log(textoModificado);
-// Salida: "Estmanzana es un ejemplomple de sustitución de elefantelefanteres."
+function desencriptarTexto(stringDesencriptada){
+    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringDesencriptada.includes(matrizCodigo[i][1])){
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1],matrizCodigo[i][0])
+        }
+    }
+    return stringDesencriptada
+}
